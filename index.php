2ec83596353b0eabd1c98b2bbdc73874dbfff38a
Administrator <admin@ip47.vs3.titanaxe.com>
2020-01-07 15:13:20 +0000

    Initial: added index and basic css

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="/css/style.css"/>
+		<meta charset="utf-8">
+		<title>Network Builder</title>
+	</head>
+	<body>
+		<div id="menu">
+		</div>
+		<div id="">
+	</body>
+</html>
