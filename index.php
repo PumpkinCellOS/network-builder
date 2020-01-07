6e01df4a781ab6f557b7d586d2cd3b8ebef269c6
Administrator <admin@ip47.vs3.titanaxe.com>
2020-01-07 15:45:04 +0000

    Created main page layout

--- index.php
+++ index.php
@@ -1,12 +1,31 @@
 <html>
 	<head>
-		<link rel="stylesheet" href="/css/style.css"/>
+		<link rel="stylesheet" href="/networkbuilder/css/style.css"/>
 		<meta charset="utf-8">
 		<title>Network Builder</title>
 	</head>
 	<body>
 		<div id="menu">
+			<div id="option-menu">
+				<div id="options-file"> <!-- new, open, save -->
+				xxx
+				</div>
+				<div id="options-objects"> <!-- devices, cables -->
+				yyy
+				</div>
+				<div id="options-program"> <!-- settings, about, help -->
+				zzz
+				</div>
+			</div>
+			<div id="object-menu"> <!-- objects -->
+			</div>
 		</div>
-		<div id="">
+		<div id="editor">
+		</div>
+		<div id="footer">
+			<div class="footer-left">Objects: <span id="fv-objcount">0</span></div>
+			<div class="footer-right">Network Builder v1.0 | Sppmacd (c) 2020</div>
+		</div>
+		<script src="/networkbuilder/js/ui.js"></script>
 	</body>
 </html>
